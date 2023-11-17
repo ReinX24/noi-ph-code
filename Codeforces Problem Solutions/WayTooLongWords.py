77f3dbec6ddb34cf1e3c6d3cd384ed4a479ee64b
Rein Solis <reinX244@gmail.com>
2023-11-17 22:08:21 +0800

    Added Codeforces Problem Solutions folder and 2 solutions.

---
+++ Codeforces Problem Solutions/WayTooLongWords.py
@@ -0,0 +1,13 @@
+wordsCount = int(input())
+
+wordsInput = []
+
+for _ in range(wordsCount):
+    wordsInput.append(input())
+
+for eachWord in wordsInput:
+    if len(eachWord) > 10:
+        newString = eachWord[1:-1]
+        print(f"{eachWord[0]}{len(newString)}{eachWord[-1]}")
+    else:
+        print(f"{eachWord}")
---
+++ NOI.ph Tutorial Problems/Peacekeeper.py
@@ -0,0 +1,6 @@
+rounds_input = int(input())
+
+if rounds_input % 2:
+    print('EVENING RUINED')
+else:
+    print('EVENING SAVED')
